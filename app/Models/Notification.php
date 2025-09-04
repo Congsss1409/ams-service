b66e1507d2e556d950f357837533ea64d42c7014
Congsss1409 <162850941+Congsss1409@users.noreply.github.com>
2025-09-04 17:29:54 +0800

    new

---
+++ app/Models/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'type',
+        'message',
+        'link',
+        'is_read',
+    ];
+
+    /**
+     * Get the user that the notification belongs to.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
