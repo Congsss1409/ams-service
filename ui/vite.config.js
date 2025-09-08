c09b60462b5310e58ad3c8f9b82598490e13dbb8
Congsss1409 <162850941+Congsss1409@users.noreply.github.com>
2025-09-08 22:56:35 +0800

    Update vite.config.js

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       // Any request starting with /api will be forwarded to the Laravel backend
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://331225b6a1e3.ngrok-free.app',
         changeOrigin: true,
         secure: false,
       },
